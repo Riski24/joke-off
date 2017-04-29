ab0e36e65aba58d427ba08c10a3c965d71f24a3c
Kyle <kyle.anson7@gmail.com>
2017-04-28 21:14:10 -0400

    Clean out old code

--- server/database/config.js
+++ server/database/config.js
@@ -24,25 +24,4 @@ var Round = db.define('rounds', {
 Competitor.belongsTo(Bracket);
 Round.belongsTo(Bracket);
 
-// var users = db.define('users', {
-//   username: {type: Sequelize.STRING, allowNull: false, unique: true}
-//   //password: {type: Sequelize.STRING, allowNull: false}
-// });
-
-// var questions = db.define('questions', {
-//   choice1: {type: Sequelize.STRING, allowNull: false},
-//   choice2: {type: Sequelize.STRING, allowNull: false},
-//   votes1: {type: Sequelize.INTEGER, defaultValue: 0},
-//   votes2: {type: Sequelize.INTEGER, defaultValue: 0}
-// });
-
-// var userQuestionVotes = db.define('user_question_votes', {
-//   userid: {type: Sequelize.INTEGER, allowNull: false},
-//   questionid: {type: Sequelize.INTEGER, allowNull: false},
-//   choice: {type: Sequelize.INTEGER, allowNull: false}
-// });
-
-// users.belongsToMany(questions, {through: 'user_question_vote'});
-// questions.belongsToMany(users, {through: 'user_question_vote'});
-
 module.exports = db;
