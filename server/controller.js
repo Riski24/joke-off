eaeeb619ccc5890372e13906157465e5c96ba28e
Kyle <kyle.anson7@gmail.com>
2017-04-28 23:26:34 -0400

    Update to new request handler (controller)

--- server/controller.js
+++ server/controller.js
@@ -1,40 +1,88 @@
 var db = require('./database/db.js');
 
 module.exports = {
-  questions: {
-    getAll: function(req, res) {
-      db.questions.findAll({raw: true})
-        .then(function(results) {
-          //if (err) { console.error(err); }
-          res.send(results);
-        });
-    },
-
-    postOne: function(req, res) {
-      db.questions.create({
-        choice1: req.body.choice1,
-        choice2: req.body.choice2
+  Bracket: {
+    createBracket: function(req, res) {
+      var adminUrl = 'PLACEHOLDER';
+      var userUrl = 'PLACEHOLDER';
+      db.Bracket.create({
+        adminUrl: adminUrl,
+        userUrl: userUrl
       })
-      .then(function(results) {
-        //if (err) { console.log(err) }
-        console.log('question accepted into database');
-        res.sendStatus(201);
+      .then(function(newBracket) {
+        res.status(201).send(newBracket);
+      })
+      .catch(function(err) {
+        console.error(err);
+        res.status(500).send();
       });
     },
-
-    vote: function(req, res) {
-      db.questions.findOne({where: {id: req.body.id}})
-        .then(function(question) {
-          console.log('choice', req.body.choice)
-          if (req.body.choice === 1) {
-            question.votes1++;
-          } else if (req.body.choice === 2) {
-            question.votes2++;
-          }
-          question.save().then(function() {
-            res.sendStatus(201);
+    findBracket: function(req, res) {
+      // Check if user url
+      db.Bracket.findOne({where: {userUrl: req.query.url}})
+      .then(function(bracket) {
+        if (!bracket) {
+          // If bracket isnt found check if admin url
+          db.Bracket.findOne({where: {adminUrl: req.query.url}})
+          .then(function(bracket) {
+            res.status(200).send({bracket: bracket, admin: true});
+          })
+          .catch(function(err) {
+            console.error(err);
+            res.status(500).send();
           });
-        });
+        } else {
+          res.status(200).send({bracket: bracket, admin: false});
+        }
+      })
+      .catch(function(err) {
+        console.error(err);
+        res.status(500).send();
+      });
+    }
+  },
+  Competitor: {
+    createCompetitor: function(req, res) {
+      db.Competitor.create({
+        bracketId: req.body.bracketId,
+        name: req.body.name
+      })
+      .then(function(newCompetitor) {
+        res.status(201).send(newCompetitor);
+      })
+      .catch(function(err) {
+        console.error(err);
+        res.status(500).send();
+      });
+    },
+    findCompetitor: function(req, res) {
+      db.Competitor.findOne({where: {id: req.query.id}})
+      .then(function(competitor) {
+        res.status(200).send(competitor);
+      })
+      .catch(function(err) {
+        console.error(err);
+        res.status(500).send();
+      });
+    }
+  },
+  Round: {
+    createRound: function(req, res) {
+      db.Round.create({
+        bracketId: req.body.bracketId,
+        competitor1: req.body.competitor1,
+        competitor2: req.body.competitor2
+      })
+    },
+    findRound: function(req, res) {
+      db.Round.findOne({where: {id: req.query.id}})
+      .then(function(round) {
+        res.status(200).send(round);
+      })
+      .catch(function(err) {
+        console.error(err);
+        res.status(500).send();
+      });
     }
   }
-}
+};
