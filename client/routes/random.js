770425f028c5f2358d2c7aca0c7458edb7961429
Kyle <kyle.anson7@gmail.com>
2017-04-30 13:40:17 -0400

    Clean out old files

--- client/routes/random.js
+++
@@ -1,8 +0,0 @@
-angular.module('app.random', [])
-
-.controller('RandomController', function($scope, Questions) {
-  Questions.getOne()
-  .then(function(data) {
-    $scope.question = data.data;
-  });
-});
