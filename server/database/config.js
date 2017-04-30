165fc200df67d2dfbbaddcbd546a985b5c2a9dbc
Kyle <kyle.anson7@gmail.com>
2017-04-30 02:22:14 -0400

    Changes to database schema

--- server/database/config.js
+++ server/database/config.js
@@ -7,21 +7,24 @@ if (process.env.DATABASE_URL) {
 }
 
 var Bracket = db.define('brackets', {
+	name: Sequelize.STRING,
   adminUrl: {type: Sequelize.STRING, allowNull: false, unique: true},
   userUrl: {type: Sequelize.STRING, allowNull: false, unique: true}
 });
 
 var Competitor = db.define('competitors', {
+	bracketId: {type: Sequelize.INTEGER, allowNull: false},
   name: Sequelize.STRING
 });
 
 var Round = db.define('rounds', {
+	bracketId: {type: Sequelize.INTEGER, allowNull: false},
   competitor1: Sequelize.INTEGER,
   competitor2: Sequelize.INTEGER,
   winner: Sequelize.INTEGER
 });
 
-Competitor.belongsTo(Bracket, {as: 'bracket'});
-Round.belongsTo(Bracket, {as: 'bracket'});
+Competitor.belongsTo(Bracket, {foreignKey: 'bracketId'});
+Round.belongsTo(Bracket, {foreignKey: 'bracketId'});
 
 module.exports = db;
