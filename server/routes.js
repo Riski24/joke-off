12569727d8861942d31a9cb1241a2163d6030873
Kyle <kyle.anson7@gmail.com>
2017-04-30 02:26:51 -0400

    Condense route names

--- server/routes.js
+++ server/routes.js
@@ -1,12 +1,12 @@
 var controller = require('./controller.js');
 
 module.exports = function(app) {
-	app.post('/api/createBracket', controller.Bracket.createBracket);
-	app.get('/api/findBracket', controller.Bracket.findBracket);
+	app.post('/api/Bracket', controller.Bracket.createBracket);
+	app.get('/api/Bracket', controller.Bracket.findBracket);
 
-	app.post('/api/createCompetitor', controller.Competitor.createCompetitor);
-	app.get('/api/findCompetitor', controller.Competitor.findCompetitor);
+	app.post('/api/Competitor', controller.Competitor.createCompetitor);
+	app.get('/api/Competitor', controller.Competitor.findCompetitor);
 
-	app.post('/api/createRound', controller.Round.createRound);
-	app.get('/api/findRound', controller.Round.findRound);
+	app.post('/api/Round', controller.Round.createRound);
+	app.get('/api/Round', controller.Round.findRound);
 };
